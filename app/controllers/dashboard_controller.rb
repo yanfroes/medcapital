4151a3c6ca07d6b7d743671450a83ffb87c9f35a
Yan <yanfroes@gmail.com>
2020-06-26 15:24:57 -0300

    trying to fix heroku bug

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,11 +3,11 @@ class DashboardController < ApplicationController
     
 def index
     @q = Transaction.search(params[:q])
-    @transactions = @q.result(distinct: true).order('created_at DESC')
+    @transactions = @q.result(distinct: true).where(user_id: current_user.id).order('created_at DESC')
     @total = Transaction.where(kind: "deposit").sum(:value) - Transaction.where(kind: "withdraw").sum(:value)
 
-    @deposits = Transaction.where(kind: "deposit").order('created_at DESC')
-    @withdraws = Transaction.where(kind: "withdraw").order('created_at DESC')
+    @deposits = Transaction.where(kind: "deposit", user_id: current_user.id).order('created_at DESC')
+    @withdraws = Transaction.where(kind: "withdraw", user_id: current_user.id).order('created_at DESC')
 end
 
 
